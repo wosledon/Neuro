adda8a67dd74de035835a97ec8f5f74e2396b3a8
ledon <powertle@outlook.com>
2026-02-02 14:46:58 +0800

    feat: 添加 RegisterRequest DTO，包含账户、密码、姓名和邮箱字段

--- src/Neuro.Api/Dtos/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Neuro.Api.Dtos;
-
-public record RegisterRequest
-{
-    public required string Account { get; init; }
-    public required string Password { get; init; }
-    public string? Name { get; init; }
-    public string? Email { get; init; }
-}
--- src/Neuro.Shared/Dtos/RegisterRequest.cs
+++ src/Neuro.Shared/Dtos/RegisterRequest.cs
@@ -4,5 +4,8 @@ namespace Neuro.Shared.Dtos;
 
 public record RegisterRequest
 {
-
+    public required string Account { get; init; }
+    public required string Password { get; init; }
+    public string? Name { get; init; }
+    public string? Email { get; init; }
 }
